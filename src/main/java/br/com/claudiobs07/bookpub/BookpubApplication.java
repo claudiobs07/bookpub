64717703623f147e5d5f8008bacd63684da7ecb8
Claudio Santos <claudiobs07@gmail.com>
2018-05-16 17:07:14 -0300

    add: project structure

---
+++ src/main/java/br/com/claudiobs07/bookpub/BookpubApplication.java
@@ -0,0 +1,12 @@
+package br.com.claudiobs07.bookpub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookpubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookpubApplication.class, args);
+	}
+}

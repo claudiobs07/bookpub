df0b638ec4cfe97cac8a368bee9129ad8fd6f1cb
Claudio Santos <claudiobs07@gmail.com>
2018-05-18 17:32:08 -0300

    add: scheduling into CommandLineRunner

--- src/main/java/br/com/claudiobs07/bookpub/BookpubApplication.java
+++ src/main/java/br/com/claudiobs07/bookpub/BookpubApplication.java
@@ -1,22 +1,18 @@
 package br.com.claudiobs07.bookpub;
 
-import br.com.claudiobs07.bookpub.command.BookCountRunner;
 import br.com.claudiobs07.bookpub.command.StartupRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class BookpubApplication {
 
     @Bean
     public StartupRunner schedulerRunner() {
 	return new StartupRunner();
-    }
-
-    @Bean
-    public BookCountRunner counterRunner() {
-	return new BookCountRunner();
     }
 
     public static void main(String[] args) {
--- src/main/java/br/com/claudiobs07/bookpub/command/StartupRunner.java
+++ src/main/java/br/com/claudiobs07/bookpub/command/StartupRunner.java
@@ -1,15 +1,26 @@
 package br.com.claudiobs07.bookpub.command;
 
+import br.com.claudiobs07.bookpub.repository.BookRepository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.scheduling.annotation.Scheduled;
 
 public class StartupRunner implements CommandLineRunner {
 
     private final Log logger = LogFactory.getLog(getClass());
 
+    @Autowired
+    BookRepository bookRepository;
+
     @Override
     public void run(String... args) throws Exception {
-        logger.info("Hello");
+        logger.info("Hello World");
+    }
+
+    @Scheduled(initialDelay = 1000, fixedRate = 10000)
+    public void run() {
+        logger.info("Number of books: " + bookRepository.count());
     }
 }
